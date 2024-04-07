137310ed1ca0c4f387abf6bad8eb54bf247ad07c
rizki <rizkiadnanfutuh@gmail.com>
2024-04-07 15:16:41 +0700

    refactor: refactor unused statefull widget cause we have state management

--- lib/module/calculator/section/display.dart
+++ lib/module/calculator/section/display.dart
@@ -3,48 +3,30 @@ import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:glassmorphism_ui/glassmorphism_ui.dart';
 import 'package:calculator/module/calculator/state/cubit/theme_cubit.dart';
+import 'package:calculator/module/calculator/component/header.dart';
 import 'package:calculator/theme.dart';
 
-class Display extends StatefulWidget {
+class Display extends StatelessWidget {
   final String text;
   const Display(this.text, {super.key});
-
-  @override
-  State<Display> createState() => _DisplayState();
-}
-
-bool _iconBool = false;
-IconData _iconLight = Icons.wb_sunny;
-IconData _iconDark = Icons.nights_stay;
-
-class _DisplayState extends State<Display> {
   @override
   Widget build(BuildContext context) {
     return Expanded(
       flex: 1,
       child: SafeArea(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            IconButton(
-              onPressed: () {
-                //  context.read<CalculatorBloc>().add(CalculatorEvent(command: command));
-                setState(() {
-                  _iconBool = !_iconBool;
-                });
-              },
-              icon: Icon(_iconBool ? _iconDark : _iconLight),
-            ),
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: AutoSizeText(
-                widget.text,
+                text,
                 minFontSize: 20,
                 maxFontSize: 80,
                 maxLines: 1,
                 textAlign: TextAlign.end,
-                style: const TextStyle(
+                style: TextStyle(
                   fontWeight: FontWeight.w100,
                   decoration: TextDecoration.none,
                   fontSize: 80,
